1ae30c8fd50a3c3fded4a4df570b5c5a08f108cf
jovrer <jovrer@hotmail.com>
2019-05-10 16:50:27 +0800

    init

--- lualib.h
+++ lualib.h
@@ -1,16 +1,55 @@
 /*
-** Libraries to be used in LUA programs
-** Grupo de Tecnologia em Computacao Grafica
-** TeCGraf - PUC-Rio
-** $Id: lualib.h,v 1.2 1994/08/24 15:29:02 celes Stab roberto $
+** $Id: lualib.h,v 1.42 2011/05/25 14:12:28 roberto Exp roberto $
+** Lua standard libraries
+** See Copyright Notice in lua.h
 */
+
 
 #ifndef lualib_h
 #define lualib_h
 
-void iolib_open   (void);
-void strlib_open  (void);
-void mathlib_open (void);
+#include "lua.h"
 
+
+
+LUAMOD_API int (luaopen_base) (lua_State *L);
+
+#define LUA_COLIBNAME	"coroutine"
+LUAMOD_API int (luaopen_coroutine) (lua_State *L);
+
+#define LUA_TABLIBNAME	"table"
+LUAMOD_API int (luaopen_table) (lua_State *L);
+
+#define LUA_IOLIBNAME	"io"
+LUAMOD_API int (luaopen_io) (lua_State *L);
+
+#define LUA_OSLIBNAME	"os"
+LUAMOD_API int (luaopen_os) (lua_State *L);
+
+#define LUA_STRLIBNAME	"string"
+LUAMOD_API int (luaopen_string) (lua_State *L);
+
+#define LUA_BITLIBNAME	"bit32"
+LUAMOD_API int (luaopen_bit32) (lua_State *L);
+
+#define LUA_MATHLIBNAME	"math"
+LUAMOD_API int (luaopen_math) (lua_State *L);
+
+#define LUA_DBLIBNAME	"debug"
+LUAMOD_API int (luaopen_debug) (lua_State *L);
+
+#define LUA_LOADLIBNAME	"package"
+LUAMOD_API int (luaopen_package) (lua_State *L);
+
+
+/* open all previous libraries */
+LUALIB_API void (luaL_openlibs) (lua_State *L);
+
+
+
+#if !defined(lua_assert)
+#define lua_assert(x)	((void)0)
 #endif
 
+
+#endif
